af530dd35afaedaf084234fa92ccebb069a9a55b
J.H.Lee <67559886+jihyoung-lee@users.noreply.github.com>
2021-01-11 01:55:40 +0900

    codingtest

---
+++ codeUp problemset 100/codeup 1080.py
@@ -0,0 +1,7 @@
+﻿a = int(input())
+sum = 0
+n = 1
+while (sum<a):
+    sum = sum+n
+    n = n + 1
+print(n-1)
---
+++ codeUp problemset 100/codeup 1083.py
@@ -0,0 +1,9 @@
+﻿a = int(input())
+sam = []
+for i in range(1,a+1):
+    if (i%3 == 0):
+        i = 'X'
+        sam.append(i)
+    else :
+        sam.append(i)
+print(' '.join(map(str, sam)))
