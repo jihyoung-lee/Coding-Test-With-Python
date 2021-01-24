0f2d13a026479e6b81eb38909aaf560d8559b601
J.H.Lee <67559886+jihyoung-lee@users.noreply.github.com>
2021-01-24 21:52:28 +0900

    1093 : [기초-1차원배열] 이상한 출석 번호 부르기1
    정보 선생님은 수업을 시작하기 전에 이상한 출석을 부른다.
    
    선생님은 출석부를 보고 번호를 부르는데,
    학생들의 얼굴과 이름을 빨리 익히기 위해 번호를 무작위(랜덤)으로 부른다.
    
    그리고 얼굴과 이름이 잘 기억되지 않는 학생들은 번호를 여러 번 불러
    이름과 얼굴을 빨리 익히려고 하는 것이다.
    
    출석 번호를 n번 무작위로 불렀을 때, 각 번호(1 ~ 23)가 불린 횟수를 각각 출력해보자.

---
+++ codeUp problemset 100/1093_16804503(AC).py
@@ -0,0 +1,10 @@
+﻿a = int(input())
+b = input().split()
+li = []
+
+for i in range(24):
+    li.append(0)
+for i in range(a):
+    li[int(b[i])] +=1
+for i in range(1,24):
+    print(li[i], end=' ')
